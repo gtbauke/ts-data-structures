e637f59a6687db0a9ef2e86d576f5a16875350b5
Gustavo T. Bauke <baukegustavo@gmail.com>
2023-09-03 17:01:04 -0300

    adds eslint and prettier

--- src/linked-list/linked-list.ts
+++ src/linked-list/linked-list.ts
@@ -1,141 +1,144 @@
-type LinkedListNode<T> = {
-    value: T;
-    next: LinkedListNode<T> | null;
-};
-
-export class IndexOutOfBounds extends Error {
-    public constructor(index: number, length: number) {
-        super(`Index ${index} is out of bounds. Container has length ${length}`);
-    }
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+interface LinkedListNode<T> {
+  value: T
+  next: LinkedListNode<T> | null
 }
 
-export class ContainerDoesNotContain<T> extends Error {
-    public constructor(element: T) {
-        super(`Container does not contain the element ${element}.`);
-    }
+const node = <T>(value: T): LinkedListNode<T> => ({ value, next: null } satisfies LinkedListNode<T>)
+
+export class IndexOutOfBounds extends Error {
+  public constructor (index: number, length: number) {
+    super(`Index ${index} is out of bounds. Container has length ${length}`)
+  }
+}
+
+export class ContainerDoesNotContain<T extends { toString: () => string }> extends Error {
+  public constructor (element: T) {
+    super(`Container does not contain the element ${element.toString()}.`)
+  }
 }
 
 export class LinkedList<T> {
-    private head: LinkedListNode<T> | null;
-    
-    public length = 0;
-    
-    public constructor(value: T | null = null) {
-        this.head = value ? { value, next: null } : null;
+  private head: LinkedListNode<T> | null
+
+  public length = 0
+
+  public constructor (value: T | null = null) {
+    this.head = value !== null ? node(value) : null
+  }
+
+  public static from<A>(container: ArrayLike<A>): LinkedList<A> {
+    const list = new LinkedList<A>()
+    const array = Array.from(container)
+
+    for (const elem of array) {
+      list.append(elem)
     }
-    
-    public static from<A>(container: ArrayLike<A>): LinkedList<A> {
-        const list = new LinkedList<A>();
-        const array = Array.from(container);
-        
-        for (const elem of array) {
-            list.append(elem);
-        }
-        
-        return list;
+
+    return list
+  }
+
+  public append (value: T): this {
+    if (this.head === null) {
+      this.head = { value, next: null }
+      this.length++
+
+      return this
     }
-    
-    public append(value: T): this {
-        if (!this.head) {
-            this.head = { value, next: null };
-            this.length++;
-            
-            return this;
-        }
-        
-        const node = { value, next: null };
-        let pointer = this.head;
-        
-        while (pointer.next) {
-            pointer = pointer.next;
-        }
-        
-        pointer.next = node;
-        this.length++;
-        
-        return this;
+
+    const node = { value, next: null }
+    let pointer = this.head
+
+    while (pointer.next !== null) {
+      pointer = pointer.next
     }
-    
-    public prepend(value: T): this {
-        if (!this.head) {
-            this.head = { value, next: null };
-            this.length++;
-            
-            return this;
-        }
-        
-        const tail = this.head;
-        this.head = { value, next: tail };
-        this.length++;
-        
-        return this;
+
+    pointer.next = node
+    this.length++
+
+    return this
+  }
+
+  public prepend (value: T): this {
+    if (this.head === null) {
+      this.head = { value, next: null }
+      this.length++
+
+      return this
     }
-    
-    /**
+
+    const tail = this.head
+    this.head = { value, next: tail }
+    this.length++
+
+    return this
+  }
+
+  /**
     * Inserts a value into a specified position in the linked list
     * @throws {IndexOutOfBounds} When the provided position is less than zero or greater or equal than the list length
     * @param value Value to be inserted in the linked list
     * @param at Index to insert new node
     * @returns The linked list with the new node inserted at the correct position
     */
-    public insert(value: T, at: number): this {
-        if (at >= this.length || at < 0) {
-            throw new IndexOutOfBounds(at, this.length);
-        }
-        
-        let before: LinkedListNode<T> | null = null;
-        let pointer = this.head;
-        let counter = 0;
-        
-        while (counter !== at) {
-            counter++;
-            before = pointer;
-            pointer = pointer!.next;
-        }
-        
-        const tail = pointer;
-        const node = { value, next: tail };
-        pointer = node;
-        before!.next = pointer;
-        
-        return this;
-    }
-    
-    public find(fn: (e: T) => boolean): T | null {
-        let pointer = this.head;
-        
-        while (pointer) {
-            if (fn(pointer.value)) {
-                return pointer.value;
-            }
-            
-            pointer = pointer.next;
-        }
-        
-        return null;
+  public insert (value: T, at: number): this {
+    if (at >= this.length || at < 0) {
+      throw new IndexOutOfBounds(at, this.length)
     }
 
-    public toString(): string {
-        const str: T[] = [];
-        
-        let pointer = this.head;
-        while (pointer) {
-            str.push(pointer.value);
-            pointer = pointer.next;
-        }
-        
-        return str.join(", ");
+    let before: LinkedListNode<T> | null = null
+    let pointer = this.head
+    let counter = 0
+
+    while (counter !== at) {
+      counter++
+      before = pointer
+      pointer = pointer!.next
     }
-    
-    public toArray(): T[] {
-        const array: T[] = [];
-        
-        let pointer = this.head;
-        while (pointer) {
-            array.push(pointer.value);
-            pointer = pointer.next;
-        }
-        
-        return array;
+
+    const tail = pointer
+    const node = { value, next: tail }
+    pointer = node
+    before!.next = pointer
+
+    return this
+  }
+
+  public find (fn: (e: T) => boolean): T | null {
+    let pointer = this.head
+
+    while (pointer !== null) {
+      if (fn(pointer.value)) {
+        return pointer.value
+      }
+
+      pointer = pointer.next
     }
+
+    return null
+  }
+
+  public toString (): string {
+    const str: T[] = []
+
+    let pointer = this.head
+    while (pointer !== null) {
+      str.push(pointer.value)
+      pointer = pointer.next
+    }
+
+    return str.join(', ')
+  }
+
+  public toArray (): T[] {
+    const array: T[] = []
+
+    let pointer = this.head
+    while (pointer !== null) {
+      array.push(pointer.value)
+      pointer = pointer.next
+    }
+
+    return array
+  }
 }
